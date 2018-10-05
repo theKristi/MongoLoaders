2c1640fc5163568d624abba6023fe5c73e6036a3
Kristi <kristithecoder@gmail.com>
2018-10-05 07:32:17 -0700

    adding git ignore and working through things

--- Hogwarts/InitLoader/setup.js
+++ Hogwarts/InitLoader/setup.js
@@ -1,1 +1,6 @@
-import {parser} 
+import {Parser} from "htmlParser.js"
+import {fs} from "../../node_modules"
+
+fs.readFile( 'index.html', 'utf8', function(err, html){
+    Parser.parse(html);
+});
