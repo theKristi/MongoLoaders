9df0ee50f9165cb064fcb96adde227570dddc57d
Kristi <kristithecoder@gmail.com>
2018-10-08 07:25:51 -0700

    getting there

--- Hogwarts/InitLoader/setup.js
+++ Hogwarts/InitLoader/setup.js
@@ -1,10 +1,22 @@
 const fs = require('fs');
-const parser =require('./parser');
-const jsdom= require("jsdom");
-fs.readFile( 'index.html', 'utf8', function(err, strhtml){
+const parser = require('tabletojson');
+
+fs.readFile( 'InitLoader/index.html', 'utf8', function(err, html){
    if(err)
     return console.log(err)
-    const dom = new jsdom(strhtml)
-    let table=dom.window.document.querySelector("[data-list-manipulate]")
-    parser(table);
+    const converted = parser.convert(html);
+    const MongoClient = require('mongodb').MongoClient;
+
+// replace the uri string with your connection string.
+const uri = "mongodb+srv://thekristi:GiveMeAdb123!@cluster0-uas4b.mongodb.net/test?retryWrites=true"
+MongoClient.connect(uri, function(err, client) {
+   if(err) {
+        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+   }
+   console.log('Connected...');
+   const collection = client.db("WizardingWorld").collection("HogwartsStudents");
+   //converted.map(())
+   client.close();
+});
+    
 });
