17fefbac59b8181717324e1444769de3ebae1390
Kristi Marks <kristithecoder@gmail.com>
2018-10-09 07:06:16 -0700

    Update setup.js

--- setup.js
+++ setup.js
@@ -8,7 +8,7 @@ fs.readFile( 'InitLoader/index.html', 'utf8', function(err, html){
     const MongoClient = require('mongodb').MongoClient;
 
 // replace the uri string with your connection string.
-const uri = "mongodb+srv://thekristi:GiveMeAdb123!@cluster0-uas4b.mongodb.net/test?retryWrites=true"
+const uri = "mongodb+srv://<USERNAME>:<PASSWORD>@cluster0-uas4b.mongodb.net/test?retryWrites=true"
 MongoClient.connect(uri, function(err, client) {
    if(err) {
         console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
